b1b7f64fe04cf309ae83c2e4ed68776a9ef20815
esign99 <ishayadennis25@gmail.com>
2022-10-18 16:45:00 -0700

    work 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet_x10(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
